0c1f27dc2ba23fde938f8a0520a383a06ac50b48
hayar.abderrafia <hayar.abderrafia@HP-PROBOOK-20.uni.lan>
2021-05-07 13:42:21 +0200

    Bestea

--- Data_base/Erlete_Script.sql
+++ Data_base/Erlete_Script.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE erlete_db;
 
 CREATE table Expense(
-    id_expense int primary key autoincrement,
+    id_expense int primary key AUTO_INCREMENT,
     description varchar(255),
     price float,
     expense_type varchar(50)
 )
 
 CREATE table Can(
-    id_can int primary key autoincrement,
+    id_can int primary key AUTO_INCREMENT,
     capacity int,
     availability boolean,
     last_use date
@@ -21,30 +21,28 @@ CREATE table Partner(
     password varchar(255),
     IBAN varchar(30),
     phone varchar(15),
-    name varchar(100)
+    address varchar(100)
 )
 create table Partership_fee(
-    partner_DNI varchar(15) primary key,
-    year int primary key,
+    partner_DNI varchar(15),
+    year int,
     fee_charged boolean,
+	PRIMARY KEY(partner_DNI, year),
     CONSTRAINT fk_partBazkideDNI FOREIGN KEY (partner_DNI) REFERENCES Partner(DNI) 
 )
 create table Production_fee(
-    partner_DNI varchar(15) primary key,
-    month varchar(15) primary key,
-    year int primary key,
+    partner_DNI varchar(15),
+    month varchar(15),
+    year int ,
+	PRIMARY KEY(partner_DNI, month, year),
     total_price float,
     CONSTRAINT fk_prodBazkideDNI FOREIGN KEY (partner_DNI) REFERENCES Partner(DNI)
 )
 create table Room_booking(
-    id_booking int primary key autoincrement,
+    id_booking int primary key AUTO_INCREMENT,
     partner_DNI varchar(15),
     book_date date,    
     extracted_quantity int,
     CONSTRAINT fk_bookBazkideDNI FOREIGN KEY (partner_DNI) REFERENCES Partner(DNI)
 )
-create table Extraction(
-    id_booking int primary key,
-    extracted_quantity int,
-    CONSTRAINT fk_extIdBooking FOREIGN KEY (partner_DNI) REFERENCES Partner(DNI)
-)
+
